ad38527af2055d1cae806743eaee0c795f84a4d3
Shevi Zak <shevizak@gmail.com>
2017-07-25 11:17:43 -0400

    sign in

--- games/main.py
+++ games/main.py
@@ -24,37 +24,44 @@ class MainHandler(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
     # If the user is logged in...
+    ""
+    main_template = env.get_template('signin.html')
+    self.response.out.write(main_template.render())
     if user:
       email_address = user.nickname()
       cssi_user = CssiUser.get_by_id(user.user_id())
-      signout_link_html = '<a href="%s">sign out</a>' % (
+      signout_link_html = '<a class = edits href="%s">sign out</a>' % (
           users.create_logout_url('/'))
       # If the user has previously been to our site, we greet them!
       if cssi_user:
-        self.response.write('''
-            Welcome %s %s (%s)! <br> %s <br>''' % (
+        self.response.write(''' <div class = edits>
+            Welcome %s %s (%s)! <br> %s <br> </div>''' % (
               cssi_user.first_name,
               cssi_user.last_name,
               email_address,
               signout_link_html))
       # If the user hasn't been to our site, we ask them to sign up
       else:
-        self.response.write('''
+        self.response.write('''<div class = edits>
             Welcome to our site, %s!  Please sign up! <br>
             <form method="post" action="/">
             <input type="text" name="first_name">
             <input type="text" name="last_name">
             <input type="submit">
-            </form><br> %s <br>
+            </form><br> %s <br> </div>
             ''' % (email_address, signout_link_html))
     # Otherwise, the user isn't logged in!
     else:
-      self.response.write('''
+      self.response.write('''<div class = edits>
         Please log in to use our site! <br>
-        <a href="%s">Sign in</a>''' % (
+        <a href="%s">Sign in</a> </div>''' % (
           users.create_login_url('/')))
 
+
+
   def post(self):
+    main_template = env.get_template('signin.html')
+    self.response.out.write(main_template.render())
     user = users.get_current_user()
     if not user:
       # You shouldn't be able to get here without being logged in
@@ -68,6 +75,9 @@ class MainHandler(webapp2.RequestHandler):
     self.response.write('Thanks for signing up, %s!' %
         cssi_user.first_name)
 
+    "</div>"
+
+
 class GameHandler(webapp2.RequestHandler):
   def get(self):
     main_template = env.get_template('mainpage.html')
@@ -79,7 +89,7 @@ class HistoryHandler(webapp2.RequestHandler):
       self.response.out.write(main_template.render())
 
 app = webapp2.WSGIApplication([
-  ('/userlogin', MainHandler),
-  ('/', GameHandler),
+  ('/', MainHandler),
+  ('/homepage', GameHandler),
   ('/history-of-gaming', HistoryHandler)
 ], debug=True)
